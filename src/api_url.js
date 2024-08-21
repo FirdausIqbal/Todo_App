215b94b1b750882a07aafe18cf625f7de755ec6b
FirdausIqbal <firdausiqbal10@gmail.com>
2024-08-22 03:27:56 +0700

    debug fix

--- src/api_url.js
+++ src/api_url.js
@@ -1,4 +1,4 @@
 
-const API_URL = process.env.ENV === "prod" ? "https://todo-backend-api-eta.vercel.app/" : "http://localhost:3001/"
+const REACT_APP_API_URL = process.env.REACT_APP_ENV === "prod" ? "https://todo-backend-api-eta.vercel.app/" : "http://localhost:3001/"
 
-export default API_URL;
+export default REACT_APP_API_URL;
